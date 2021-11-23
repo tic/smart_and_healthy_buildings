cd3d41f256042a2ad91e303d062b6c0c476658ef
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-11-23 16:52:46 -0500

    stop scanning after enough beacons have reported and compute avg dist

--- lab_5/code.py
+++ lab_5/code.py
@@ -10,17 +10,25 @@ from adafruit_ble import BLERadio
 from adafruit_ble.advertising import Advertisement, to_hex
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 
-ble = BLERadio()
-print("scanning")
 found = set()
 scan_responses = set()
+beacon_distances = {}
 
+# variables to control how BLE packets are filtered
 DATA_TYPE = 255 # "manufacturer specific"
 MANUFACTURER = b'\x4c\x00' # Apple
 APPLE_DATA_TYPE = 2 # iBeacon data
 ATTENUATION = 0.1 # integer in [2, 6] to adjust for environmental circumstances
 CALIBRATION = 1 # value to scale distance measurements
 
+# variables to control how many beacons we need to hear from, and how much we
+# need to hear from each beacon, before attempting position triangulation
+BEACON_COUNT = 6
+BEACON_DIST_COUNT = 6
+
+
+print("scanning")
+ble = BLERadio()
 # By providing Advertisement as well we include everything, not just specific advertisements.
 for advertisement in ble.start_scan(ProvideServicesAdvertisement, Advertisement):
 
@@ -44,6 +52,11 @@ for advertisement in ble.start_scan(ProvideServicesAdvertisement, Advertisement)
     TXPower = adbytes[29]
     RSSI = advertisement.rssi
 
+    # if this is the first time seeing this beacon, initialize it's list of
+    # observed positions
+    if MACAddress not in beacon_distances:
+        beacons[MACAddress] = []
+
     # exponent = float(RSSI - TXPower) / float(10*ATTENUATION)
     # estimatedDistance = (10**exponent) * CALIBRATION
 
@@ -51,9 +64,37 @@ for advertisement in ble.start_scan(ProvideServicesAdvertisement, Advertisement)
     exponent = (raw_exponent + 2) / -15
     estimatedDistance = (2.718 ** exponent) * CALIBRATION
 
-    # look for specific calibration sensors
-    # if 'e7' in MACAddress or '74' in MACAddress or '69' in MACAddress:
-    print(MACAddress, TXPower, RSSI, exponent, estimatedDistance)
+    # only print debug data for specific calibration sensors
+    if 'e7' in MACAddress or '74' in MACAddress or '69' in MACAddress:
+        print(MACAddress, TXPower, RSSI, exponent, estimatedDistance)
 
+    # record that we heard from the beacon
+    beacon_distances[MACAddress].append(estimatedDistance)
 
-print("scan done")
+    # keep listening for more beacon advertisements if we haven't heard from
+    # enough beacons yet
+    found_beacons = len(beacon_distances)
+    if found_beacons < BEACON_COUNT:
+        continue
+
+    # if we have heard from enough beacons, verify that each beacon has reported
+    # enough distance data
+    keep_going = False
+    for distances in beacon_distances.values():
+        if len(distances) < BEACON_DIST_COUNT:
+            keep_going = True
+            break
+
+    if keep_going is False:
+        break
+
+print("scan done. beacons accquired!")
+
+print("computing average distances")
+from statistics import mean
+for beacon, distances in beacon_distances.items():
+    beacons[beacon] = mean(distances)
+print("done. beacon distances computed")
+
+print("triangulating position using heard nodes")
+# todo!
